516a294f68112672865002395f29a929b2632622
womendexinshijie <womendexinshijie@sina.com>
2018-03-05 10:53:55 +0800

    Initial Commit

---
+++ BahamaAirLoginScreenOCTransition/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BahamaAirLoginScreenOCTransition
+//
+//  Created by qinglinyou on 2018/3/5.
+//  Copyright © 2018年 qinglinyou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BahamaAirLoginScreenOCTransition/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BahamaAirLoginScreenOCTransition
+//
+//  Created by qinglinyou on 2018/3/5.
+//  Copyright © 2018年 qinglinyou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
